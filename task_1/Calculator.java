78a92f52a8da9e91b0b46292c7fdb5beabae5548
unknown <AnfinogenovPavel>
2024-11-04 23:22:39 +0500

    task_1/Calculator.java

--- task_1/Calculator.java
+++ task_1/Calculator.java
@@ -1,55 +1,34 @@
-package task_1;
-
-public class Calculator {
+public static class Calculator {
   // Обобщенный метод для сложения
-  public static <T extends Number> T sum(T num1, T num2) {
-    if (num1 instanceof Integer) {
-      return (T) Integer.valueOf(num1.intValue() + num2.intValue());
-    } else if (num1 instanceof Double) {
-      return (T) Double.valueOf(num1.doubleValue() + num2.doubleValue());
-    } else {
-      throw new IllegalArgumentException("Unsupported data type");
+    public static <T extends Number> double sum(T num1, T num2) {
+      return num1.doubleValue() + num2.doubleValue();
     }
-  }
 
   // Обобщенный метод для умножения
-  public static <T extends Number> T multiply(T num1, T num2) {
-    if (num1 instanceof Integer) {
-      return (T) Integer.valueOf(num1.intValue() * num2.intValue());
-    } else if (num1 instanceof Double) {
-      return (T) Double.valueOf(num1.doubleValue() * num2.doubleValue());
-    } else {
-      throw new IllegalArgumentException("Unsupported data type");
+    public static <T extends Number> double multiply(T num1, T num2) {
+      return num1.doubleValue() * num2.doubleValue();
     }
-  }
 
   // Обобщенный метод для деления
-  public static <T extends Number> T divide(T num1, T num2) {
-    if (num1 instanceof Integer) {
-      return (T) Double.valueOf(num1.intValue() / num2.intValue());
-    } else if (num1 instanceof Double) {
-      return (T) Double.valueOf(num1.doubleValue() / num2.doubleValue());
-    } else {
-      throw new IllegalArgumentException("Unsupported data type");
+    public static <T extends Number> double divide(T num1, T num2) {
+    if (num2.doubleValue() == 0) {
+      throw new IllegalArgumentException("Деление на ноль не допускается");
     }
+    return num1.doubleValue() / num2.doubleValue();
   }
 
+
   // Обобщенный метод для вычитания
-  public static <T extends Number> T subtract(T num1, T num2) {
-    if (num1 instanceof Integer) {
-      return (T) Integer.valueOf(num1.intValue() - num2.intValue());
-    } else if (num1 instanceof Double) {
-      return (T) Double.valueOf(num1.doubleValue() - num2.doubleValue());
-    } else {
-      throw new IllegalArgumentException("Unsupported data type");
+    public static <T extends Number> double subtract(T num1, T num2) {
+    return num1.doubleValue() - num2.doubleValue();
     }
   }
 
   public static void main(String[] args) {
     // Примеры использования
-    int intSumResult = Calculator.sum(5, 3);
+    int intSumResult = (int) Calculator.sum(5, 3);
     double doubleDivideResult = Calculator.divide(10.0, 2.5);
-    int intSubtractResult = Calculator.subtract(10, 5);
+    int intSubtractResult = (int) (int) Calculator.subtract(10, 5);
     double doubleMultiplyResult = Calculator.multiply(3.5, 2.0);
 
     System.out.println("Сумма: " + intSumResult);
@@ -57,5 +36,4 @@ public class Calculator {
     System.out.println("Разность: " + intSubtractResult);
     System.out.println("Произведение: " + doubleMultiplyResult);
   }
-}
 
